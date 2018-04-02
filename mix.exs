bac0c55c498d994948cd09ba951109c3f879d637
Sam Davies <sampdavies@gmail.com>
2018-04-02 16:39:07 +0100

    Bump to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rihanna.MixProject do
   def project do
     [
       app: :rihanna,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
