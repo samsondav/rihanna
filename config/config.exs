ed6cc1be9b8a0dd4a66bc08578b5895ec27b0dd8
Sam Davies <sampdavies@gmail.com>
2018-04-09 21:36:15 +0100

    Add .travis.yml

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,23 @@
 use Mix.Config
 
-config :rihanna,
-  jobs_table_name: "rihanna_jobs",
-  postgrex: [
-    username: "sam",
-    password: "",
-    database: "rihanna_db",
-    hostname: "127.0.0.1",
-    port: 5432
-  ]
+if System.get_env("TRAVIS") == "true" do
+  config :rihanna,
+    jobs_table_name: "rihanna_jobs",
+    postgrex: [
+      username: "postgres",
+      password: "",
+      database: "rihanna_test_db",
+      hostname: "127.0.0.1",
+      port: 5432
+    ]
+else
+  config :rihanna,
+    jobs_table_name: "rihanna_jobs",
+    postgrex: [
+      username: "sam",
+      password: "",
+      database: "rihanna_db",
+      hostname: "127.0.0.1",
+      port: 5432
+    ]
+end
