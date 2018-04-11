8fb4ee6cb6efc356d04752ac38add48c1268e288
Sam Davies <sampdavies@gmail.com>
2018-04-11 10:11:05 +0100

    Support Elixir 1.5 officially

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rihanna.MixProject do
     [
       app: :rihanna,
       version: "0.4.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
