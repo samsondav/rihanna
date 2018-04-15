6650ef8fcc9996a0eec5d353964ad8e6cdfb4daf
Sam Davies <sampdavies@gmail.com>
2018-04-15 14:11:58 +0100

    Bump version from 0.5.0 to 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rihanna.MixProject do
   def project do
     [
       app: :rihanna,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
