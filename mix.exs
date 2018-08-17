acaeb1ed91016f32f36b04af626ce4e44cadc689
Sam Davies <sampdavies@gmail.com>
2018-08-17 09:19:55 +0100

    Not trivial to support Elixir 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rihanna.MixProject do
     [
       app: :rihanna,
       version: "0.7.2",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
@@ -35,7 +35,7 @@ defmodule Rihanna.MixProject do
     [
       {:postgrex, ">= 0.13.3"},
       # Development tools
-      {:benchee, "~> 0.13", only: :test},
+      {:benchee, ">= 0.13", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false}
